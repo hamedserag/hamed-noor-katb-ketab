21802f3e0baea83acc09e6ba2b04979db1ff7dde
Hamed Serag <60942745+hamedserag@users.noreply.github.com>
2026-08-29 21:38:30 +0300

    Add editable caption field for guest album photos

--- server/schema.sql
+++ server/schema.sql
@@ -50,6 +50,7 @@ CREATE TABLE IF NOT EXISTS site_images (
 CREATE TABLE IF NOT EXISTS guest_photo_uploads (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     guest_name VARCHAR(160) NULL,
+    caption VARCHAR(180) NULL,
     original_name VARCHAR(255) NOT NULL,
     stored_name VARCHAR(255) NULL,
     mime_type VARCHAR(100) NOT NULL,
